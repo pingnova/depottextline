f411857db67483a98bf7838132a23cfb5f91befa
forest <forest.n.johnson@gmail.com>
2022-10-25 14:21:14 -0500

    cleanup

--- schema_migrations/02_down_messages_numbers.sql
+++
@@ -1,4 +0,0 @@
-DROP TABLE messages;
-DROP TABLE remote_numbers;
-
-UPDATE schemaversion SET version = 1;
--- schema_migrations/03_down_accounts.sql
+++
@@ -1,7 +0,0 @@
-
-DROP TABLE login_tokens;
-DROP TABLE accounts;
-
-UPDATE schemaversion SET version = 2;
-
-
