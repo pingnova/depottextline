1d4f9542346b07b95deda8d9cb416fd88598dc4a
forest <forest.n.johnson@gmail.com>
2022-10-28 18:25:00 -0500

    EventHub reconnect logic

--- frontend/src/EventHub.js
+++ frontend/src/EventHub.js
@@ -2,9 +2,12 @@
 
 
 const EventHub = {
+  originalReconnectTimeoutMs: 100,
+  reconnectTimeoutMs: 100,
   startStreamingIfNotAlreadyStarted: () => {
     if(!EventHub.sse) {
       EventHub.sse = new EventSource('/events/stream');
+
       EventHub.sse.addEventListener("message", (e) => {
         const eventData = JSON.parse(e.data);
         console.log(eventData);
@@ -17,6 +20,22 @@ const EventHub = {
             }
           )
         )
+      });
+
+      EventHub.sse.addEventListener("open", (e) => {
+        EventHub.reconnectTimeoutMs = EventHub.originalReconnectTimeoutMs;
+      });
+
+      EventHub.sse.addEventListener("error", (e) => {
+        EventHub.sse.close();
+        setTimeout(() => {
+          EventHub.reconnectTimeoutMs = EventHub.reconnectTimeoutMs * 2;
+          if(EventHub.reconnectTimeoutMs > 10000) {
+            EventHub.reconnectTimeoutMs = 10000;
+          }
+          EventHub.sse = null;
+          EventHub.startStreamingIfNotAlreadyStarted();
+        });
       });
     }
   },
